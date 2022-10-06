c162c9d57178e5aa1e713424f50e51d31504f1a6
Mariele Mota <115173457+MarieleMota@users.noreply.github.com>
2022-10-06 15:51:56 +0000

    Initial commit

---
+++ conversor.py
@@ -0,0 +1,12 @@
+def conversor():
+  valor_cm = float(input('Digite um valor em CENTÍMETROS: '))
+
+  valor_pol = valor_cm / 2.54
+
+  msg = open('Texto.txt', 'w+')
+  msg.write('\nO valor {} em centímetros, corresponde a {:.2f} em polegadas' .format(valor_cm, valor_pol))
+
+  msg.seek(0.0)
+  print(msg.read())
+
+conversor()
